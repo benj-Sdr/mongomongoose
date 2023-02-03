6c4693262e2d308568abd196b70fab67ddb164bf
Biniam Tefrie <79765727+beni1218@users.noreply.github.com>
2023-02-03 17:50:48 +0200

    Conncect mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-
+//require mongoose
+const mongoose = require('mongoose')
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
