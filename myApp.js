de1d74b00db5ad9661407ae4f08103f16cddf70e
Biniam Tefrie <79765727+beni1218@users.noreply.github.com>
2023-02-03 21:02:24 +0200

    Solved problem 1

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 //require mongoose
 const mongoose = require('mongoose')
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(process.env.MONGO_URI);
 let Person;
 
 const createAndSavePerson = (done) => {
